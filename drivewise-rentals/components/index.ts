a55b46a3ae49c6ed208f31151cb8e86fc32c7da8
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-05-14 12:55:52 -0700

    built starting nav bar, footer WIP

--- drivewise-rentals/components/index.ts
+++ drivewise-rentals/components/index.ts
@@ -1,7 +1,11 @@
+import Navbar from './Navbar';
 import Hero from './Hero';
+import Footer from './Footer';
 import CustomButton from './CustomButton';
 
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer,
 }
