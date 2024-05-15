faba1af89e8a47990a0f1e17cf8046481a522622
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-05-15 12:45:57 -0700

    built footer nav as well as fix hero background image position

--- drivewise-rentals/components/Hero.tsx
+++ drivewise-rentals/components/Hero.tsx
@@ -33,8 +33,8 @@ const Hero = () => {
                     fill 
                     className='object-contain'
                 />
-                <div className='hero__image-overlay'/>
             </div>
+            <div className='hero__image-overlay'/>
         </div>
     </div>
   )
--- drivewise-rentals/components/Navbar.tsx
+++ drivewise-rentals/components/Navbar.tsx
@@ -10,16 +10,18 @@ const Navbar = () => {
         <nav className='max-w-[1440px] mx-auto flex justify-between items-center px-6 sm:px-16 py-4'>
             <Link href='/' className='flex justify-center items-center'>
                 <Image 
-                    src="/logo.svg" 
-                    alt="DriveWise Logo" 
-                    width={118} 
-                    height={18} 
-                    className='object-contain'/>
+                  src="/logo.svg" 
+                  alt="DriveWise Logo" 
+                  width={118} 
+                  height={18} 
+                  className='object-contain'
+                />
             </Link>
             <CustomButton 
-                title='Sign In'
-                btnType='button'
-                containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'/>
+              title='Sign In'
+              btnType='button'
+              containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+            />
         </nav>
     </header>
   )
