1cbce21e752411c3ebce57fb0ba3fd298d81e218
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-05-17 17:01:23 -0700

    built a car manufacturer filtered search bar (WIP)

---
+++ drivewise-rentals/components/Catalogue.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { SearchBar, CustomFilter } from '@/components';
+
+const Catalogue = () => {
+  return (
+    <div className='mt-12 padding-x padding-y max-width' id='discover'>
+      <div className='home__text-container'>
+        <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+        <p>Explore the cars you might like</p>
+      </div>
+      <div className='home__filters'>
+        <SearchBar/>
+        <div className='home__filters-container'>
+          <CustomFilter title='fuel'/>
+          <CustomFilter title='year'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Catalogue
--- drivewise-rentals/components/index.ts
+++ drivewise-rentals/components/index.ts
@@ -2,10 +2,18 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Footer from './Footer';
 import CustomButton from './CustomButton';
+import Catalogue from './Catalogue';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
+    Catalogue,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
