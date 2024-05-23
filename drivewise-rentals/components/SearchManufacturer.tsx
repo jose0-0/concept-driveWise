598a35f57489744e966c368c8e95366a018a6ca8
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-05-23 09:36:52 -0700

    removed commented out snippet

--- drivewise-rentals/components/SearchManufacturer.tsx
+++ drivewise-rentals/components/SearchManufacturer.tsx
@@ -20,56 +20,6 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
 
   return (
     <div className='search-manufacturer'>
-        {/* <Combobox>
-            <div className='relative w-full'>
-                <ComboboxButton className='absolute top-[14px]'>
-                    <Image 
-                        src='/car-logo.svg'
-                        alt='Car Logo'
-                        width={20}
-                        height={20}
-                        className='ml-4'
-                    />
-                </ComboboxButton>
-                <ComboboxInput 
-                    className='search-manufacturer__input' 
-                    placeholder='Volkswagen...'
-                    displayValue={(item: string) => item }
-                    onChange={(e) => (e.target.value)}
-                />
-                <Transition 
-                    as={Fragment}
-                    leave='transition ease-in duration-100'
-                    leaveFrom='opacity-100'
-                    leaveTo='opacity-0'
-                    afterLeave={() => setQuery("")}
-                >
-                    <ComboboxOptions>
-                        {filteredManufacturers.length === 0 &&
-                        query !== "" ? (
-                            <ComboboxOption
-                                value={query}
-                                className='search-manufacturer__option'
-                            >
-                                Create '{query}'
-                            </ComboboxOption>
-                        ): ( filteredManufacturers.map((item) => (
-                            <ComboboxOption
-                                key={item}
-                                className={({ focus }) => `
-                                relative search-manufacturer__option
-                                ${focus ? 'bg-primary-blue text-white' : 'text-gray-900'}
-                                `}
-                                value={item}
-                            >
-                                {item}
-                            </ComboboxOption>
-                        )))}
-                    </ComboboxOptions>
-                </Transition>
-            </div>
-        </Combobox> */}
-
         <Combobox value={manufacturer} onChange={setManufacturer} onClose={() => setQuery('')}>
             <div className='relative w-full'>
                 <ComboboxButton className='absolute top-[14px]'>
