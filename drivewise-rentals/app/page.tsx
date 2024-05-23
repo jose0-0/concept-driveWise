38c6c02db942748ffe688ebdf98a7df2a1e8a59a
Jose Arguetaq <jose.argueta@ionnovate.com>
2024-05-23 13:28:11 -0700

    set up api, built car cards, fixed weird margins for catalogue

--- drivewise-rentals/app/page.tsx
+++ drivewise-rentals/app/page.tsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import { Hero } from '@/components'
-import { Catalogue } from '@/components';
+import { Catalogue, CarCard } from '@/components';
+import fetchCars from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
       <Catalogue />
+
+      {!isDataEmpty ? (
+        <section className='padding-x max-width'>
+          <div className='home__cars-wrapper'>
+            {allCars?.map((car) => <CarCard car={car}/> )}
+          </div>
+        </section>
+      ): (
+        <div className='home__error-container'>
+          <h2 className='text-black text-xl font-bold'>Oops, We don't have cars</h2>
+          <p>{allCars?.message}</p>
+        </div>
+      )}
     </main>
   );
 }
